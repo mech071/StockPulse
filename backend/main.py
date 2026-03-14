0fde3034aebe2f44a5dcd2c089eb392be88beb19
mech071 <suprit2snehasis@gmail.com>
2026-03-14 12:23:37 +0530

    fixed bug

--- backend/main.py
+++ backend/main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="StockPulse API")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "https://stock-pulse-c8hc6iydz-mech071s-projects.vercel.app/",
+        "https://stock-pulse-c8hc6iydz-mech071s-projects.vercel.app",
         "http://127.0.0.1:5173"
     ],
     allow_credentials=True,
